cc8546729669505a532d1089fe4c54a657be6996
C-Build <codecode139@gmail.com>
2022-03-01 18:03:27 +0300

    Add project files.

---
+++ Data_Folder/Startup_Folder/Run_Manger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIVE_Engine.Data_Folder.Startup_Folder
+{
+
+    public class Run_Manger
+    {
+
+        public static void Run_Manger_VIVE_Engine_Luncher()
+        {
+
+        }
+
+        public static void Run_Manger_VIVE_Engine_Demo()
+        {
+
+        }
+
+    }
+
+}
---
+++ Data_Folder/Startup_Folder/Startup_Manger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIVE_Engine.Data_Folder.Startup_Folder
+{
+
+    public class Startup_Manger
+    {
+
+        public static void Startup_Manger_Start()
+        {
+
+        }
+
+    }
+
+}
